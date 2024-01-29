c318740aae15b6542312d1ee312395499c18705c
ymengsea <141946428+ymengsea@users.noreply.github.com>
2024-01-29 22:55:25 +0700

    done client review section

--- javascript/index.js
+++ javascript/index.js
@@ -1,10 +1,28 @@
-$(".owl-carousel").owlCarousel({
+$("#owl-carousel-banner").owlCarousel({
   loop: true,
   margin: 10,
   nav: false,
   autoplay: true,
   animateOut: "fadeOut",
   dots: false,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 1,
+    },
+    1000: {
+      items: 1,
+    },
+  },
+});
+// owl for review section
+$("#owl-carousel-review").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: false,
+  autoplay: true,
   responsive: {
     0: {
       items: 1,
