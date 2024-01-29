997dd8ce9d000bbe070ab5594d7a3152b30b7ec8
ymengsea <141946428+ymengsea@users.noreply.github.com>
2024-01-29 07:37:31 +0700

    done navbar and home banner

---
+++ javascript/index.js
@@ -0,0 +1,19 @@
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: false,
+  autoplay: true,
+  animateOut: "fadeOut",
+  dots: false,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 1,
+    },
+    1000: {
+      items: 1,
+    },
+  },
+});
