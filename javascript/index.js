84f7d32c1b827408795b6cbc0722dcf6227f889c
ymengsea <141946428+ymengsea@users.noreply.github.com>
2024-02-01 21:01:54 +0700

    this still have bug banner section

--- javascript/index.js
+++ javascript/index.js
@@ -2,7 +2,7 @@ $("#owl-carousel-banner").owlCarousel({
   loop: true,
   margin: 10,
   nav: false,
-  autoplay: true,
+  // autoplay: true,
   animateOut: "fadeOut",
   dots: false,
   responsive: {
@@ -22,7 +22,7 @@ $("#owl-carousel-review").owlCarousel({
   loop: true,
   margin: 10,
   nav: false,
-  autoplay: true,
+  // autoplay: true,
   responsive: {
     0: {
       items: 1,
