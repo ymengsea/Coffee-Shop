2815442ed1d7ccdf4e9254a7b65b7650a9696c27
ymengsea <141946428+ymengsea@users.noreply.github.com>
2024-02-03 23:21:59 +0700

    almost done

--- javascript/index.js
+++ javascript/index.js
@@ -1,9 +1,11 @@
+// banner section
 $("#owl-carousel-banner").owlCarousel({
   loop: true,
   margin: 10,
   nav: false,
   autoplay: true,
   animateOut: "fadeOut",
+  autoplayTimeout: 3000,
   dots: false,
   responsive: {
     0: {
@@ -61,3 +63,18 @@ $(document).ready(function () {
     },
   });
 });
+// wow animation
+var wow = new WOW({
+  boxClass: "wow", // animated element css class (default is wow)
+  animateClass: "animated", // animation css class (default is animated)
+  offset: 0, // distance to the element when triggering the animation (default is 0)
+  mobile: true, // trigger animations on mobile devices (default is true)
+  live: true, // act on asynchronously loaded content (default is true)
+  callback: function (box) {
+    // the callback is fired every time an animation is started
+    // the argument that is passed in is the DOM node being animated
+  },
+  scrollContainer: null, // optional scroll container selector, otherwise use window,
+  resetAnimation: true, // reset animation on end (default is true)
+});
+wow.init();
